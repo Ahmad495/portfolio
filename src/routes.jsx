979714d4b608e782d3fb135a738e746805c6c4d5
Ahmad495 <amdagha01@gmail.com>
2023-07-01 01:19:44 +0500

    minor fix

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Landing from './pages/landing';
+import Landing from './pages/Landing';
 
 export const router = createBrowserRouter([{ path: '/', element: <Landing /> }]);
